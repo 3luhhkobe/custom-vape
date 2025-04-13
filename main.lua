130de99ed30ec7b6ee10323026116d2cd17b815e
3luhhkobe <walker12stephenjr@gmail.com>
2025-04-13 19:40:11 -0400

    Delete main.lua

--- main.lua
+++
@@ -1,4 +0,0 @@
-print("[Custom Vape] Loaded main.lua successfully!")
-
--- Optional: load GUI
-loadstring(game:HttpGet("https://raw.githubusercontent.com/YOUR_USERNAME/custom-vape/main/guis/example_gui.lua"))()
