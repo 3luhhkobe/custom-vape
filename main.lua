1abd6cd2f2218b1858ce1fae455dc2aedbd4ded9
3luhhkobe <walker12stephenjr@gmail.com>
2025-04-13 19:36:28 -0400

    Add files via upload

---
+++ main.lua
@@ -0,0 +1,4 @@
+print("[Custom Vape] Loaded main.lua successfully!")
+
+-- Optional: load GUI
+loadstring(game:HttpGet("https://raw.githubusercontent.com/YOUR_USERNAME/custom-vape/main/guis/example_gui.lua"))()
